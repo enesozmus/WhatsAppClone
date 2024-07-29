e42bd07e7e4f4173b5c55b562b3b989e6fcbc857
enesozmus <ensozmus@gmail.com>
2024-07-29 14:50:04 +0300

    Initial Commit

---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by enesozmus on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
