c244f21a3265bdf66596eb8ae5acfacd88522faf
enesozmus <ensozmus@gmail.com>
2024-07-31 21:37:25 +0300

    Episode 12 | Installing Firebase SDKs

--- WhatsAppClone/App/WhatsAppCloneApp.swift
+++ WhatsAppClone/App/WhatsAppCloneApp.swift
@@ -6,9 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
+
+/// We Need To Install The Following Firebase Dependencies:
+/// FirebaseAuth
+/// FirebaseDatabase
+/// FirebaseDatabaseSwift
+/// FirebaseMessaging
+/// FirebaseStorage
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct WhatsAppCloneApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
